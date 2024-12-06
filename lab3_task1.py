629a6c03f3588d23fb9e731450153b0423475af6
testerplay <kulagin.test2@gmail.com>
2024-12-06 16:29:26 +0200

    first commit

---
+++ lab3_task1.py
@@ -0,0 +1,32 @@
+def Odd(K):
+    """
+    K - integer number.
+    Return True if K is odd, otherwise return False.
+    """
+    return K % 2 != 0
+
+def CountOddNumbers(in_lst):
+    """
+    in_lst - list of integers.
+    Return the count of odd numbers in the list.
+    """
+    count = 0
+    for num in in_lst:
+        if Odd(num):
+            count += 1
+    return count
+
+def task2():
+    """
+    Input data, call function for counting odd numbers
+    output results.
+    """
+    in_data = []
+    for i in range(5):
+        temp = int(input("Number: "))
+        in_data.append(temp)
+    odd_count = CountOddNumbers(in_data)
+    print("Count of odd numbers: ", odd_count)
+
+# Call the task2 function to execute the program
+task2()
